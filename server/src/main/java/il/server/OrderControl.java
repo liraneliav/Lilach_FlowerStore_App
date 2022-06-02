e8b45df8766efb6d3809b0a1a68ae3f4f7a70dbf
roieshahar <roie26296@gmail.com>
2022-06-03 01:23:03 +0300

    added changes to cancelOrder need ido's approval

--- server/src/main/java/il/server/OrderControl.java
+++ server/src/main/java/il/server/OrderControl.java
@@ -9,30 +9,39 @@ public class OrderControl {
     public static void cancelOrder(int id){
         testDB.openSssion();
         Order a = testDB.session.get(Order.class, id);
-//        checkRefunds(a);
         testDB.session.delete(a);
         testDB.session.flush();
         testDB.session.getTransaction().commit(); // Save everything.
         testDB.closeSession();
     }
-        /** added 2 func for refunds*/
-
-//    private static void checkRefunds(Order a) {
+    /** added 3 func for refunds*/
+//    public static void deleteOrder(Order a){
+//        testDB.openSssion();
+//        testDB.session.delete(a);
+//        testDB.session.flush();
+//        testDB.session.getTransaction().commit(); // Save everything.
+//        testDB.closeSession();
+//    }
+//
+//    public static void cancelOrder(int id){
+//        testDB.openSssion();
+//        Order a = testDB.session.get(Order.class, id);
+//        testDB.closeSession();
 //        String current_date = java.time.LocalDate.now().toString();
-//        if (current_date.equals(a.getDateReceive())) {
-//            int current_time = timeToInt(java.time.LocalTime.now().toString());
-//            int order_time = timeToInt(a.getTimeReceive());
-//            if (order_time - current_time < 300) {
-//                if (order_time - current_time > 60) {
-//                    //a.get 50% refunds
-//                }
-//                return;
-//            }
-//            //a.get 100% refunds
+//        int current_time = timeToInt(java.time.LocalTime.now().toString());
+//        int order_time = timeToInt(a.getTimeReceive());
+//        if (current_date.equals(a.getDateReceive()) && order_time - current_time < 300) {
+//            if (order_time - current_time > 60)
+//                //a.get 50% refunds
+//            deleteOrder(a);
 //            return;
 //        }
 //        //a.get 100% refunds
+//        deleteOrder(a);
+//        return;
 //    }
+//
+//
 //    public static int timeToInt(String time){
 //        time = time.replace(":", "");
 //        time = time.substring(0,3);
